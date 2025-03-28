7735563dc952197eec56f9cc77c7ee03da1186db
Ekaterina-Kolesova <m2408550@edu.misis.ru>
2025-03-28 20:48:30 +0300

    blue_3

---
+++ Lab_8/Lab_8/Blue_3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    public class Blue_3 : Blue
+    {
+        public Blue_3(string input) : base(input) { }
+
+        public override void Rewiew()
+        {
+
+        }
+    }
+}

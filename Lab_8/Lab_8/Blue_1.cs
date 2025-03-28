102aa6c07fcdc40005bcd68d6255db3a7457cf81
Ekaterina-Kolesova <m2408550@edu.misis.ru>
2025-03-28 22:56:39 +0300

    Blue_1

--- Lab_8/Lab_8/Blue_1.cs
+++ Lab_8/Lab_8/Blue_1.cs
@@ -8,11 +8,55 @@ namespace Lab_8
 {
     public class Blue_1 : Blue
     {
-        public Blue_1(string input) : base(input) { }
+        private string[] _output;
+        public string[] Output => _output;
+        public Blue_1(string input) : base(input)
+        {
+            _output = new string[0];
+        }
+
+        private void AddToOutput(string str)
+        {
+            Array.Resize(ref _output, _output.Length + 1);
+            _output[_output.Length - 1] = str;
+        }
+        private string SplitOne(string str)
+        {
+            if (str.Length < 50)
+            {
+                AddToOutput(str);
+                return null;
+            }
+
+            // ищем индекс разбития counter
+            int counter = 50;
+            while (!Char.IsWhiteSpace(str[counter]))
+                counter--;
+            // сохраняем в res первые counter индексов из str
+            char[] resAsArray = new char[counter];
+            char[] strAsArray = str.ToCharArray();
+            Array.Copy(strAsArray, resAsArray, counter);
+            string res = new string(resAsArray);
+            // добавляем res в outrut
+            AddToOutput(res);
+            // удаляем res + пробел из str
+            str = str.Remove(0, counter + 1);
+
+            return str;
+        }
 
         public override void Rewiew()
         {
-            
+            string tmp = Input;
+            while(!String.IsNullOrEmpty(tmp)) 
+                tmp = SplitOne(tmp);
+        }
+        public override void ToString()
+        {
+            for(int k = 0; k < _output.Length; k++)
+            {
+                Console.WriteLine(_output[k]);
+            }
         }
     }
 }

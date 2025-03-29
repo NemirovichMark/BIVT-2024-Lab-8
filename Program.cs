ad17eb3781b20a0b8dfc8d605b101de71410eeaf
Mover-R <denis.tugow2014@gmail.com>
2025-03-29 15:54:41 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIVT_2024_Lab_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

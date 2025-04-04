0ca128a6bb6aaf6036a4efe8b2b46c3122e37f03
Andy <obrezkov.andy@gmail.com>
2025-04-04 20:31:37 +0300

    Add Purple_4

--- Lab_8/obj/Debug/net8.0/Lab_8.AssemblyInfo.cs
+++ Lab_8/obj/Debug/net8.0/Lab_8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6776f7aba47cd6478ccf4ca07c594f3cb3385d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6806d7caba1b7bd1bea6c9cdd418ef8ec30ef2c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

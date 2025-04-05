7f1ba1222b7db7dbe8e450e43fe702ba810fd999
Ekaterina-Kolesova <m2408550@edu.misis.ru>
2025-04-05 21:02:12 +0300

    Program.cs

--- Lab_8/Lab_8/Program.cs
+++ Lab_8/Lab_8/Program.cs
@@ -15,6 +15,9 @@ namespace Lab_8
             //blue1.Review();
             //Console.WriteLine(blue1.ToString());
             //Console.WriteLine();
+            //Blue_1 blue_1 = new Blue_1(null);
+            //blue_1.Review();
+            //Console.WriteLine(blue_1.ToString());
 
             // 2
             //Blue_2 blue2 = new Blue_2("Дано текст. Необходимо удалить из текста все слова," +
@@ -27,6 +30,9 @@ namespace Lab_8
             //blue2.Review();
             //Console.WriteLine(blue2.ToString());
             //Console.WriteLine();
+            //Blue_2 blue_2 = new Blue_2("ddfddf" , null);
+            //blue_2.Review();
+            //Console.WriteLine(blue_2.ToString());
 
             // 3
             //Blue_3 blue3 = new Blue_3("Определить долю (в процентах) слов, начинающихся на различные буквы." +
@@ -36,12 +42,18 @@ namespace Lab_8
             //    " Метод ToString() должен возвращать построчно пары символа и вещественного числа, разделенных “-”.");
             //blue3.Review();
             //Console.WriteLine(blue3.ToString());
+            //Blue_3 blue_3 = new Blue_3(null);
+            //blue_3.Review();
+            //Console.WriteLine(blue_3.ToString());
 
             // 4
             //Blue_4 blue4 = new Blue_4("Текст234 соде-23ржит45 слова45 и це765лые чи-09сла произвольного порядка." +
             //    " Найти сумму включенных в текст чисел.");
             //blue4.Review();
             //Console.WriteLine(blue4.ToString());
+            Blue_4 blue_4 = new Blue_4("");
+            blue_4.Review();
+            Console.WriteLine(blue_4.ToString());
 
         }
     }

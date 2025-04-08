80bc2ebf060cbd1eac85ad0f8b7b9c8c85d1e208
Mover-R <denis.tugow2014@gmail.com>
2025-04-08 18:47:36 +0300

    init

---
+++ Purple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    abstract class Purple
+    {
+        private string _input;
+        protected char[] _separators = { '.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/' };
+        public string Input { get { return _input; } }
+        public Purple(string input)
+        {
+            _input = input;
+        }
+
+        protected bool IsSeparator(char c)
+        {
+            foreach(var s  in _separators) if (s == c) return true;
+            return false;
+        }
+
+        public abstract void Review();
+        public abstract string ToString();
+    }
+}
---
+++ Purple_3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    internal class Purple_3 : Purple
+    {
+        private string _output = null;
+        public string Output => _output;
+        public Purple_3(string input) : base(input) { }
+        public override void Review()
+        {
+
+        }
+        public override string ToString()
+        {
+            var s = Input;
+            string[] words = new string[0];
+            string word = "";
+            foreach (var letter in s)
+            {
+                if (IsSeparator(letter))
+                {
+                    if (word.Length != 0) Append(ref words, new string(word.Reverse().ToArray()));
+                    Append(ref words, letter.ToString());
+                    word = "";
+                }
+                else
+                {
+                    word += letter;
+                }
+            }
+
+            if (word.Length != 0) Append(ref words, new string(word.Reverse().ToArray()));
+
+            string res = "";
+            foreach (var w in words)
+            {
+                res += w;
+            }
+
+            return res;
+        }
+        private void Append(ref string[] words, string word)
+        {
+            Array.Resize(ref words, words.Length + 1);
+            words[words.Length - 1] = word;
+        }
+    }
+}

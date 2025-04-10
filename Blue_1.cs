b8c513f85c93d99cbfab7badafbe6caa8e3788ec
Ggh13 <62508921+Ggh13@users.noreply.github.com>
2025-04-10 11:29:49 +0300

    git push --force main

---
+++ Blue_1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab_8
+{
+    public class Blue_1 : Blue
+    {
+        string[] output;
+        public string[] Output
+        {
+            get
+            {
+                return output;
+            }
+        }
+
+        public Blue_1(string input) : base(input)
+        {
+            output = new string[0];
+        }
+
+
+        public override void Review()
+        {
+
+            int n = 0;
+            string lastSave = "";
+            string curSave = "";
+            string[] newRes = new string[0];
+            
+            for (int i = 0; i < Input.Length; i++)
+            {
+                Console.WriteLine(Input[i]);
+                if (Input[i] == ' ')
+                {
+                    curSave += ' ';
+                    if (curSave.Length < 50)
+                    {
+                        lastSave = curSave;
+                    }
+                    else
+                    {
+                        Array.Resize(ref newRes, newRes.Length + 1);
+                        newRes[newRes.Length - 1] = lastSave;
+                        curSave = curSave.Substring(lastSave.Length, curSave.Length - lastSave.Length); 
+                        lastSave = "";
+                    }
+                }
+                else
+                {
+                    curSave += Input[i];
+                }
+                
+
+            }
+            
+            output = newRes;
+            ToString();
+
+        }
+
+        public string ToString()
+        {
+            string result = "";
+            for(int i = 0; i < output.Length; i++)
+            {
+                if(i == output.Length - 1)
+                {
+                    result += output[i];
+                }
+                else
+                {
+                    result += output[i] + '\n';
+                }
+                
+
+            }
+            return result;
+        }
+    }
+}
---
+++ Blue_3.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    public class Blue_3 : Blue
+    {
+
+        private (char letter, double percentage)[] output;
+        private readonly string _text;
+
+        public Blue_3(string text): base(text)
+        {
+            
+        }
+
+        public (char letter, double percentage)[] Output => output;
+
+        public override string ToString()
+        {
+            var output = Output;
+            return string.Join(Environment.NewLine, output.Select(x => $"{x.letter} - {x.percentage}"));
+        }
+
+
+        public override void Review()
+        {
+
+            var words = Input.Split(new[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length == 0)
+                return;
+
+            var letterCounts = new Dictionary<char, int>();
+
+            foreach (var word in words)
+            {
+                if (word == null)
+                    continue;
+
+                char firstChar = char.ToLower(word[0]);
+                if(alphabet.IndexOf(firstChar) == -1)
+                {
+                    continue;
+                }
+                if (letterCounts.ContainsKey(firstChar))
+                {
+                    letterCounts[firstChar]++;
+                }
+                else
+                {
+                    letterCounts[firstChar] = 1;
+                }
+                Console.WriteLine(word);
+            }
+            var totalWords = 0;
+            /*
+            foreach (var word in letterCounts)
+            {
+                totalWords += word.Value;
+                Console.Write(word.Key);
+                Console.Write(" - ");
+                Console.WriteLine(word.Value);
+            }
+            Console.WriteLine(totalWords);
+            foreach (var word in letterCounts)
+            {
+                
+                Console.Write(word.Key);
+                Console.Write(" - ");
+                Console.WriteLine(word.Value / ((double)totalWords / 100));
+            }
+            */
+            var result = letterCounts
+                .Select(kv => (letter: kv.Key, percentage:  (double)kv.Value / ((double)totalWords / 100)))
+                .OrderByDescending(x => x.percentage)
+                .ThenBy(x => x.letter)
+                .ToArray();
+
+            double totalPercentage = result.Sum(x => x.percentage);
+            
+            //Console.WriteLine(totalPercentage);
+
+            output = result;
+        }
+    }
+}

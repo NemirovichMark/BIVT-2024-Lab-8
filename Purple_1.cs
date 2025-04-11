ac45e833f8b92cd34aab269f43a85bd5c68ae4a1
Anastasia-Hlystova <m2408317@edu.misis.ru>
2025-04-11 18:18:41 +0300

    Add files via upload

---
+++ Purple_1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    public class Purple_1 : Purple
+    {
+        private string _output;
+        public string Output => _output;
+        public Purple_1(string input) : base(input) 
+        {
+            _output = "";
+        }
+        public override void Review()
+        {
+            if (_input == null) return;
+            var result = new StringBuilder();
+            punctuation.Append(' ').ToArray();
+            var splited = _input.Split(punctuation);
+            int i = 0;
+            foreach (string slovo in splited)
+            {
+                if (IsWord(slovo))
+                {
+                    char[] perevorot = slovo.ToCharArray();
+                    Array.Reverse(perevorot);
+                    string naoborot = new string(perevorot);
+                    result.Append(naoborot);
+                }
+                else
+                {
+                    result.Append(slovo);
+                }
+                i += slovo.Length + 1;
+                if (i < _input.Length)
+                {
+                    result.Append(_input[i]);
+                }
+            }
+
+            _output = result.ToString();
+
+        }
+        public override string ToString()
+        {
+            return _output;
+        }
+    }
+}
---
+++ Purple_4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    public class Purple_4 : Purple
+    {
+        public string Output { get; private set; }
+        private (string, char)[] _codes;
+
+        public Purple_4(string input, (string, char)[] codes) : base(input)
+        {
+            _codes = codes;
+        }
+        public override void Review()
+        {
+            if(Input == null || _codes == null) return;
+            StringBuilder result = new StringBuilder(Input);
+            foreach (var code in _codes)
+            {
+                if(code.Item1 == null || code.Item2 == default(char))
+                {
+                    continue;
+                }
+                result = result.Replace(code.Item2.ToString(), code.Item1);
+                Output = result.ToString();
+            }
+        }
+        public override string ToString()
+        {
+            return Output;
+        }
+    }
+}
